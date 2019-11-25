a089c1e5420a643ddd024fa27311f34292fa88ab
alberto56 <albert@mediatribe.net>
2019-11-25 18:24:17 -0500

    Better deploy script, better organize files and add poster image for video

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 set -e
 
-docker run -dit --name antichars -p 8080:80 -v "$PWD/docs":/usr/local/apache2/htdocs/ httpd:2.4
+./scripts/build-static-site.sh
+
+./scripts/destroy.sh
+docker run --rm -dit --name antichars -p 8080:80 -v "$PWD/docs/_site":/usr/local/apache2/htdocs/ httpd:2.4
 
 echo ""
 echo "Visitez http://0.0.0.0:8080 pour voir le site localement."
